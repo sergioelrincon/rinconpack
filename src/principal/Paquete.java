fee16c6132ed978f69447f34d6e6602a6dfd757e
Sergio Ramos <sergioramos@ieselrincon.es>
2026-04-09 09:50:06 +0100

    Primera versión de la clase Paquete

--- src/principal/Paquete.java
+++ src/principal/Paquete.java
@@ -4,10 +4,50 @@
  */
 package principal;
 
+import java.util.Objects;
+
 /**
  *
  * @author Sergio Ramos
  */
 public class Paquete {
+    private String codigo;
+    private double peso;
+    private String destino;
+
+    public Paquete(String codigo, double peso, String destino) {
+        this.codigo = codigo;
+        this.peso = peso;   
+        this.destino = destino;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.codigo);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Paquete other = (Paquete) obj;
+        return Objects.equals(this.codigo, other.codigo);
+    }
+
+    @Override
+    public String toString() {
+        return "Paquete{" + "codigo=" + codigo + ", peso=" + peso + ", destino=" + destino + '}';
+    }
+    
+    
     
 }
